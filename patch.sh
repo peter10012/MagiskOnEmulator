6a7fae10148a577387605aba0b521f96aa9fd100
dkmos2016 <dkmos2016@126.com>
2023-07-19 18:00:41 +0800

    update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
 adb wait-for-device
-adb -e push ramdisk.img /data/local/tmp/ramdisk.img.gz
-[[ -f magisk.zip ]] && adb -e push magisk.zip /data/local/tmp/magisk.zip
-[[ -f magisk.apk ]] && adb -e push magisk.apk /data/local/tmp/magisk.zip
-adb -e push busybox /data/local/tmp/
-adb -e push process.sh /data/local/tmp/
-adb -e shell "dos2unix /data/local/tmp/process.sh"
-adb -e shell "sh /data/local/tmp/process.sh /data/local/tmp $1"
-adb -e pull /data/local/tmp/ramdisk.img
+target=/data/local/tmp
+
+[ -f "ramdisk.img" ] && adb -e push ramdisk.img $target/ramdisk.img.gz || {
+    echo "ERROR: no ramdisk.img" && exit 1
+}
+
+[ -f "busybox" ] && adb -e push busybox $target/busybox || {
+    echo "ERROR: no busybox" && exit 0
+}
+
+if [ ! -f magisk.apk ]; then
+    ver=v26.0
+    echo "not exists, try to download magisk-${ver}..."
+    curl -L https://github.com/topjohnwu/Magisk/releases/download/${ver}/Magisk-${ver}.apk -o magisk.apk
+fi
+
+adb -e push magisk.apk $target/magisk.zip
+adb -e push process.sh $target
+adb -e shell "dos2unix $target/process.sh"
+adb -e shell "sh $target/process.sh $target $1"
+adb -e pull $target/ramdisk.img
